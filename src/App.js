362faf39909afddd8cd6277e517d8ef8ddf9d78a
Himanshu <mehra3228@gmail.com>
2023-02-03 14:57:53 +0530

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import '../src/index.css'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-sans">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="border-b border-b-gray-200 py-5">
+      <div className="max-w-7xl mx-auto px-3 flex items-center justify-start">
+        <h1 className="font-extrabold text-3xl text-gray-800">
+          <Link to="/">Rent or Sell</Link>
+        </h1>
+        <Link to="/login" className="btn btn-ghost mr-4 ml-auto px-7">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-primary px-7">
+          Signup
+        </Link>
+      </div>
+    </header>
+  );
+}
+export default Header;
